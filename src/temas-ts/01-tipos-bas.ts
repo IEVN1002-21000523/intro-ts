c13d9a8b2ec0fa173de06b0741b83ee2879294a4
Galilea Ferrer porras <sakuramamilla@gmail.com>
2024-09-07 10:04:58 -0600

    primer commit de intro-ts

---
+++ src/temas-ts/01-tipos-bas.ts
@@ -0,0 +1,17 @@
+//
+var num1=23;
+let num2='Juan';
+const num3 =34;
+
+const sum = num1+num2 + num3;
+console.log( `the sum is:${sum}`)
+console.log('Hola mundo')
+//definir variables 
+let nombre:string='Juan'
+let edad:number =23;
+let activo:boolean = true;
+//puedes definir mas de una en una variable 
+let matircula:number|string;
+matircula=21000523
+matircula='gto210232'
+
