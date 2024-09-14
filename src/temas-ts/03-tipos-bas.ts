c1aea7e0604a216e38a99b0ad3720ecb6714d892
Galilea Ferrer porras <sakuramamilla@gmail.com>
2024-09-13 20:12:16 -0600

    viernes 13 de septiebre

--- src/temas-ts/03-tipos-bas.ts
+++ src/temas-ts/03-tipos-bas.ts
@@ -1,16 +1,31 @@
 //funciones
+
 function sumar(){
         var num1 =6;
         var num2 =6;
         var resultado = num1+num2;
         console.log(`EL resultado es:+ ${resultado}`)
+       
 }
+
+function dospuntos(){
+    var x1 =-3;
+    var x2=-5;
+    var y1=6;
+    var y2=1;
+    var resultado =Math.pow((x2-x1),2)+Math.pow((y2-y1),2);
+    console.log(`EL resultado es:+ ${resultado}`)
+}
+
+
 //
 function restar(a:number,b:number):number{
     return a- b ;
 }
 const res=restar(10,2)
 console.log(res)
+
+
 
 interface Mascota{
     nombre:string;
